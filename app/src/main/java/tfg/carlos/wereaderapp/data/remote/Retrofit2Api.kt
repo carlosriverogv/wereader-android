101e02528add752d31e05a91e0931526bd568f8d
carlosriverogv <karlos.riverog@gmail.com>
2026-03-14 20:09:41 +0100

    Modificación para desarrollo en local

--- app/src/main/java/tfg/carlos/wereaderapp/data/remote/Retrofit2Api.kt
+++ app/src/main/java/tfg/carlos/wereaderapp/data/remote/Retrofit2Api.kt
@@ -13,7 +13,7 @@ import tfg.carlos.wereaderapp.data.remote.api.SharedLibraryService
 import tfg.carlos.wereaderapp.data.remote.api.UserService
 
 object Retrofit2Api  {
-    private const val BASE_URL = "http://wereader.es:80/"
+    private const val BASE_URL = "http://192.168.1.139:3000/" // http://wereader.es:80/
 
     private val sessionManager: SessionManager = WeReaderApplication.sessionManager
 
